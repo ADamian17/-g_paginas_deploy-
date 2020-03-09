d421ce187179f2e553502385b9f8f90b2e274d93
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2020-03-08 21:14:20 -0700

    Updates

---
+++ precache-manifest.d1642af26165bd48074c10edcc654d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4fc903e84f9a920de20b98e361f87f",
+    "url": "/g_paginas_deploy%20/index.html"
+  },
+  {
+    "revision": "49e63d4d84c648d88f02",
+    "url": "/g_paginas_deploy%20/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98a94da6f4eee1938da1",
+    "url": "/g_paginas_deploy%20/static/js/2.86dbb765.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/g_paginas_deploy%20/static/js/2.86dbb765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e63d4d84c648d88f02",
+    "url": "/g_paginas_deploy%20/static/js/main.b27e21cc.chunk.js"
+  },
+  {
+    "revision": "7533961206bd1724ebad",
+    "url": "/g_paginas_deploy%20/static/js/runtime-main.182e5650.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/g_paginas_deploy%20/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/g_paginas_deploy%20/precache-manifest.d1642af26165bd48074c10edcc654d67.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/g_paginas_deploy%20/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
